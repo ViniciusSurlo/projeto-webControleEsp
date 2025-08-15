0232de2813cd9619c0493e52d1fd047c17d587a0
Vinícius Surlo Gama <vinisurlo@gmail.com>
2025-08-15 12:03:30 -0300

    Subindo ao GitHub

--- Frontend/utils.js
+++ Frontend/utils.js
@@ -1,3 +1,4 @@
 // export const enderecoServidor = 'http://localhost:3000';
 // export const enderecoServidor = 'https://webcontroleesp-six.vercel.app';
 export const enderecoServidor = 'http://10.130.42.2:3000';
+// Teste
